ce33171d2c399eb80aae2aaae1d78272a983cb88
Koichi Nakanishi <keizo042dev@gmail.com>
2019-06-18 14:14:05 +0900

    Add Config

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/morikuni/failure"
+)
+
+type Config struct {
+	GithubToken string `toml:"github_token"`
+}
+
+func Parse(path string) (*Config, error) {
+	var c Config
+	if _, err := toml.DecodeFile(path, &c); err != nil {
+		return nil, failure.Wrap(err)
+	}
+	return &Config{}, nil
+}
+
+func (c *Config) validate() error {
+	if c.GithubToken == "" {
+		return fmt.Errorf("github_token is required")
+	}
+	return nil
+}
